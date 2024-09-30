942739a844501bd654e9d37c9f7c695d9fb50151
Atamyrat Babayev <babayev.atamyrat@gmail.com>
2024-10-01 00:58:47 +0500

    chore(theme): add font assets

---
+++ lib/generated/assets.gen.dart
@@ -0,0 +1,30 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsFontsGen {
+  const $AssetsFontsGen();
+
+  /// File path: assets/fonts/fredoka_light.ttf
+  String get fredokaLight => 'assets/fonts/fredoka_light.ttf';
+
+  /// File path: assets/fonts/fredoka_medium.ttf
+  String get fredokaMedium => 'assets/fonts/fredoka_medium.ttf';
+
+  /// File path: assets/fonts/fredoka_regular.ttf
+  String get fredokaRegular => 'assets/fonts/fredoka_regular.ttf';
+
+  /// List of all assets
+  List<String> get values => [fredokaLight, fredokaMedium, fredokaRegular];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsFontsGen fonts = $AssetsFontsGen();
+}
