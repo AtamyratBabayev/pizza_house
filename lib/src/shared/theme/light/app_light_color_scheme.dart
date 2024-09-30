5cd8e1981284824c4333aa80c6eef4b41206040d
Atamyrat Babayev <babayev.atamyrat@gmail.com>
2024-10-01 01:03:28 +0500

    chore(theme): add light color scheme

---
+++ lib/src/shared/theme/light/app_light_color_scheme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../app_colors.dart';
+
+class AppLightColorScheme {
+  AppLightColorScheme._();
+
+  static ColorScheme get data {
+    return const ColorScheme(
+        brightness: Brightness.light,
+        primary: AppColors.orange,
+        onPrimary: Colors.white,
+        secondary: AppColors.orange,
+        onSecondary: Colors.white,
+        error: AppColors.red,
+        onError: Colors.white,
+        errorContainer: AppColors.redDark,
+        onErrorContainer: Colors.white,
+        surface: Colors.white,
+        onSurface: Colors.black,
+        onSurfaceVariant: AppColors.greyDark);
+  }
+}
