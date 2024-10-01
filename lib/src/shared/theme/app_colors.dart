e7b0460d3629741b6c9c1200072607dd7f04f60d
Atamyrat Babayev <babayev.atamyrat@gmail.com>
2024-10-01 15:26:53 +0500

    chore(theme): update colors

--- lib/src/shared/theme/app_colors.dart
+++ lib/src/shared/theme/app_colors.dart
@@ -4,9 +4,11 @@ class AppColors {
   AppColors._();
 
   static const Color red = Color(0xFFCC0000);
+  static const Color black = Color(0xFF202020);
   static const Color redDark = Color(0xFF7f0101);
-  static const Color orange = Color(0xFFFF983D);
-  static const Color orangeLight = Color(0xFFFFF3E3);
+  static const Color orange = Color(0xFFFFA451);
+  static const Color orangeLight = Color(0xFFFFF2E7);
   static const Color greyLight = Color(0xFFF3F4F9);
-  static const Color greyDark = Color(0xFF83838D);
+  static const Color silver = Color(0xFF86869E);
+  static const Color purpleDark = Color(0xFF27214D);
 }
