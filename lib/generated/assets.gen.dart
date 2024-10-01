f2a0074d35d2150640e3c29d68fa225d54db92e2
Atamyrat Babayev <babayev.atamyrat@gmail.com>
2024-10-02 02:46:20 +0500

    chore(assets): add heart icon, add plus icon, add demo images

--- lib/generated/assets.gen.dart
+++ lib/generated/assets.gen.dart
@@ -49,15 +49,56 @@ class $AssetsIconsGen {
   /// File path: assets/icons/flag_uk.svg
   SvgGenImage get flagUk => const SvgGenImage('assets/icons/flag_uk.svg');
 
+  /// File path: assets/icons/heart_outlined.svg
+  SvgGenImage get heartOutlined =>
+      const SvgGenImage('assets/icons/heart_outlined.svg');
+
   /// File path: assets/icons/menu.svg
   SvgGenImage get menu => const SvgGenImage('assets/icons/menu.svg');
+
+  /// File path: assets/icons/plus.svg
+  SvgGenImage get plus => const SvgGenImage('assets/icons/plus.svg');
 
   /// File path: assets/icons/search.svg
   SvgGenImage get search => const SvgGenImage('assets/icons/search.svg');
 
   /// List of all assets
-  List<SvgGenImage> get values =>
-      [basket, chevronLeft, eye, eyeOff, filters, flagUk, menu, search];
+  List<SvgGenImage> get values => [
+        basket,
+        chevronLeft,
+        eye,
+        eyeOff,
+        filters,
+        flagUk,
+        heartOutlined,
+        menu,
+        plus,
+        search
+      ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/demo_dish_1.png
+  AssetGenImage get demoDish1 =>
+      const AssetGenImage('assets/images/demo_dish_1.png');
+
+  /// File path: assets/images/demo_dish_2.png
+  AssetGenImage get demoDish2 =>
+      const AssetGenImage('assets/images/demo_dish_2.png');
+
+  /// File path: assets/images/demo_dish_3.png
+  AssetGenImage get demoDish3 =>
+      const AssetGenImage('assets/images/demo_dish_3.png');
+
+  /// File path: assets/images/demo_dish_4.png
+  AssetGenImage get demoDish4 =>
+      const AssetGenImage('assets/images/demo_dish_4.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values =>
+      [demoDish1, demoDish2, demoDish3, demoDish4];
 }
 
 class Assets {
@@ -65,6 +106,88 @@ class Assets {
 
   static const $AssetsFontsGen fonts = $AssetsFontsGen();
   static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
 }
 
 class SvgGenImage {
