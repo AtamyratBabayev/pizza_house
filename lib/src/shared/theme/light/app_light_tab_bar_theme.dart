d0b1a4cbc8ef7ef9be576032607983078b81d344
Atamyrat Babayev <babayev.atamyrat@gmail.com>
2024-10-01 19:55:37 +0500

    chore(theme): add tabbar theme

---
+++ lib/src/shared/theme/light/app_light_tab_bar_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../app_colors.dart';
+import '../app_text_theme.dart';
+
+const _paddingRightIndicator = 10.0;
+const _borderWidthIndicator = 2.0;
+
+class AppLightTabBarTheme {
+  AppLightTabBarTheme._();
+
+  static TabBarTheme get data {
+    final textTheme = AppTextTheme.data;
+    return TabBarTheme(
+        labelStyle: textTheme.headlineSmall?.copyWith(
+            color: AppColors.purpleDark, fontWeight: FontWeight.w500),
+        unselectedLabelStyle: textTheme.titleMedium
+            ?.copyWith(color: AppColors.silver, fontWeight: FontWeight.w500),
+        indicatorSize: TabBarIndicatorSize.label,
+        indicator: const UnderlineTabIndicator(
+          borderSide:
+              BorderSide(width: _borderWidthIndicator, color: AppColors.orange),
+          insets: EdgeInsets.only(right: _paddingRightIndicator),
+        ),
+        dividerHeight: 0.0);
+  }
+}
--- lib/src/shared/theme/light/app_light_theme.dart
+++ lib/src/shared/theme/light/app_light_theme.dart
@@ -5,6 +5,7 @@ import '../app_colors.dart';
 import '../app_text_theme.dart';
 import 'app_light_action_icons_theme.dart';
 import 'app_light_color_scheme.dart';
+import 'app_light_tab_bar_theme.dart';
 import 'app_light_text_input_theme.dart';
 import 'app_light_top_bar_theme.dart';
 
@@ -17,6 +18,7 @@ class AppLightTheme {
       hintColor: AppColors.silver,
       colorScheme: AppLightColorScheme.data,
       useMaterial3: true,
+      tabBarTheme: AppLightTabBarTheme.data,
       actionIconTheme: AppLightActionIconsTheme.data,
       appBarTheme: AppLightTopBarTheme.data,
       fontFamily: FontFamily.brandonGrotesque,
