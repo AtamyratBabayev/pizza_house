8d97ddcb34e940b43f3a999b635a88f16971b767
Atamyrat Babayev <babayev.atamyrat@gmail.com>
2024-10-02 02:51:26 +0500

    chore: add dish model

---
+++ lib/src/features/dishes/models/dish.dart
@@ -0,0 +1,12 @@
+class Dish {
+  const Dish(
+      {required this.id,
+      required this.name,
+      required this.image,
+      required this.price});
+
+  final int id;
+  final String name;
+  final String image;
+  final double price;
+}
